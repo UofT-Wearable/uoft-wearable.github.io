5c310236d39e4ea2efd1ed3612eeb9c367a74aae
JahahShin <jaehah.shin@mail.utoronto.ca>
2024-07-10 10:29:12 +0900

    new version

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -11,19 +13,17 @@ app.use(bodyParser.json());
 app.post('/send-email', (req, res) => {
     const { name, email, message } = req.body;
 
-    // Create a transporter object using SMTP transport
     let transporter = nodemailer.createTransport({
-        service: 'Gmail', // you can use any email service
+        service: 'Gmail',
         auth: {
-            user: 'uoftwearable@gmail.com', 
-            pass: 'uoftwearable2024', 
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
         },
     });
 
-    // Set up email data
     let mailOptions = {
         from: email,
-        to: 'uoftwearable@gmail.com', // your email address
+        to: process.env.RECEIVER_EMAIL,
         subject: `Contact form submission from ${name}`,
         text: `You have a new contact form submission from:
         Name: ${name}
@@ -31,7 +31,6 @@ app.post('/send-email', (req, res) => {
         Message: ${message}`,
     };
 
-    // Send mail
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             return console.log(error);
